d3f677820d0cb9e4baae3b7a0fcfa5adb9f4c78a
JPBlancoDB <jpblancodb@gmail.com>
2019-11-18 12:34:42 +0100

    Third step: Creating the Login Component

--- src/app/login/login-routing.module.ts
+++ src/app/login/login-routing.module.ts
@@ -1,16 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login.component';
 
 const routes: Routes = [
   {
     path: '',
-    loadChildren: () => import('./login/login.module').then(m => m.LoginModule),
-    data: { preload: true }
+    component: LoginComponent
   }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forChild(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule {}
+export class LoginRoutingModule {}
